d2d5a39b95a0f0ee4a153366d92fcad5b87246d1
Sugary <guo.yang@hypers.com>
2015-10-12 13:08:29 +0800

    为datalistView添加了国际化配置

---
+++ src/plugins/datalistview/locale/zh_CN.js
@@ -0,0 +1,12 @@
+define({
+    "sProcessing": '<i class="fa fa-spinner fa-spin big"></i>&nbsp;&nbsp;加载中',
+    "sLengthMenu": "每页 _MENU_ 条 ",
+    "oPaginate": {
+        "sPrevious": "上一页",
+        "sNext": "下一页",
+        "sFirst": "第一页",
+        "sLast": "最后一页"
+    },
+    "sInfo": "共 <span class='num'>_TOTAL_</span> 条数据",
+    "sEmptyTable": "<i class='fa fa-info-circle  big'></i>  查询结果为空",
+});
