6facd74d3310c7c76e9a16b877e368884569e92e
simonguo <simonguo.2009@gmail.com>
2015-11-30 09:57:38 +0800

    更新echart图例

--- src/plugins/echarts/chart/bar.js
+++ src/plugins/echarts/chart/bar.js
@@ -8,8 +8,23 @@ define(function(require, exports, module) {
 
     module.exports = function(options) {
 
-        var dataList = options.data || [];
-        var config = options.config || {};
+
+        var dataList = options.data || [],
+            config = options.config || {},
+            //行数据
+            rows = options.rows,
+            //列标签
+            columns = options.columns,
+
+            //水平显示图表
+            horizon = options.horizon || false,
+
+            //值显示的列
+            valueColumn = horizon ? "xAxis" : "yAxis",
+
+            //标签显示的列
+            lableColumn = horizon ? "yAxis" : "xAxis";
+
         var option = {
             title: {
                 x: "center",
@@ -29,46 +44,96 @@ define(function(require, exports, module) {
                 show: false
             },
             calculable: false,
-            xAxis: [{
-                type: 'category',
-                data: [],
-                boundaryGap: true,
-                axisLine: {
-                    lineStyle: {
-                        color: '#CCC'
-                    }
-                },
-                splitLine: {
-                    show: false,
-                    lineStyle: {
-                        color: '#DADADA'
-                    }
+            series: []
+        };
+
+        option[valueColumn] = [{
+            name: '',
+            type: 'value',
+            nameTextStyle: {
+                color: '#000'
+            },
+            axisLine: {
+                lineStyle: {
+                    color: '#CCC'
                 }
-            }],
-            yAxis: [{
-                name: '',
-                type: 'value',
-                nameTextStyle: {
-                    color: '#000'
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: '#CCC'
-                    }
-                },
-                splitLine: {
-                    lineStyle: {
-                        color: 'rgba(240, 240, 240, 0.8)'
-                    }
-                },
-                splitArea: {
-                    show: true,
-                    areaStyle: {
-                        color: ['rgba(250,250,250,0.1)', 'rgba(200,200,200,0.1)']
-                    }
+            },
+            splitLine: {
+                lineStyle: {
+                    color: 'rgba(240, 240, 240, 0.8)'
                 }
-            }],
-            series: [{
+            },
+            splitArea: {
+                show: true,
+                areaStyle: {
+                    color: ['rgba(250,250,250,0.1)', 'rgba(200,200,200,0.1)']
+                }
+            }
+        }];
+
+
+        option[lableColumn] = [{
+            type: 'category',
+            data: [],
+            boundaryGap: true,
+            axisLine: {
+                lineStyle: {
+                    color: '#CCC'
+                }
+            },
+            splitLine: {
+                show: false,
+                lineStyle: {
+                    color: '#DADADA'
+                }
+            }
+        }];
+
+        //初始化数据(兼容老的数据格式)
+        if (dataList.length) {
+            return compatible(option);
+        }
+
+        //新的数据格式
+        if (rows && rows.length) {
+
+            //初始化数据
+            if (rows.length > 1) {
+                option.legend = {
+                    data: []
+                };
+
+            }
+            for (i = 0; i < rows.length; i++) {
+                if (option.legend) {
+                    option.legend.data.push(rows[i].name);
+                }
+
+                var series = {
+                    name: rows[i].name,
+                    type: 'bar',
+                    data: rows[i].value,
+                    itemStyle: {
+                        normal: {
+                            borderRadius: 0
+                        }
+                    }
+                };
+
+                //如果还需要设置其他参数，通过继承row上面的参数都继承到series
+                $.extend(series, rows[i]);
+                option.series.push(series);
+            }
+            option[lableColumn][0].data = options.columns;
+        }
+
+        this.option = option;
+
+
+        //兼容以前老的data格式
+        function compatible(option) {
+
+            option.series.push({
                 name: '',
                 type: 'bar',
                 itemStyle: {
@@ -77,42 +142,22 @@ define(function(require, exports, module) {
                     }
                 },
                 data: []
-            }]
-        };
+            });
 
-
-        var i, _temp;
-
-        //横向柱状图
-        if (!options.horizon) {
-
-            _temp = option.xAxis;
-            option.xAxis = option.yAxis;
-            option.yAxis = _temp;
-            //初始化数据
             for (i = 0; i < dataList.length; i++) {
-                option.yAxis[0].data.push(dataList[i].name);
+                option[lableColumn][0].data.push(dataList[i].name);
                 option.series[0].data.push(dataList[i].value);
             }
             if (options.name) {
-                option.xAxis[0].name = options.name;
+                option[valueColumn][0].name = options.name;
                 option.series[0].name = options.name;
             }
-            this.option = option;
             return option;
         }
 
-        //初始化数据
-        for (i = 0; i < dataList.length; i++) {
-            option.xAxis[0].data.push(dataList[i].name);
-            option.series[0].data.push(dataList[i].value);
-        }
-        if (options.name) {
-            option.yAxis[0].name = options.name;
-            option.series[0].name = options.name;
-        }
-        this.option = option;
         return option;
     };
+
+
 
 });
