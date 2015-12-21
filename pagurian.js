9ffc67ed5177b0c780fca9e34d74755bb0fda4ab
simonguo <simonguo.2009@gmail.com>
2015-12-21 22:58:47 +0800

    修改版本号

--- pagurian.js
+++ pagurian.js
@@ -29,7 +29,7 @@
      * @type {Object}
      */
     var pagurian = {
-        version: CONFIG.version || "1.5.2",
+        version: CONFIG.version || "1.5.3",
         language: CONFIG.language || "zh_CN", //简体中文:zh_CN , 英文:en_US
         util: {},
         com: {},
