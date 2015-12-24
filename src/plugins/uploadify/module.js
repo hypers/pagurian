7d04f1533586a4c4091eee8163d5dc20aad1a8bd
simonguo <simonguo.2009@gmail.com>
2015-12-24 11:06:26 +0800

    更新uploadify说明

--- src/plugins/uploadify/module.js
+++ src/plugins/uploadify/module.js
@@ -36,8 +36,8 @@ define(function(require, exports, module) {
             },
             /**
              * 新增的一个onRemove事件
-             * 和onCancel不同点在于:onCancel之后在上次文件的过程中触发，
-             * onRemove不管是上次过程中还是上次完成，只有点击了删除按钮都会触发
+             * 和onCancel不同点在于:onCancel上次文件的过程中(未完成上次)的时候触发，
+             * onRemove不管是上次过程中还是已经上次完成，只有点击了删除按钮都会触发。
              */
             onRemove: function(file) {
                 $p.log(file + " has been removed");
