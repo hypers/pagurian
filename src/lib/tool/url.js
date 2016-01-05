891523aaa6981fba3f7581c0a74b9746c26fa752
simonguo <simonguo.2009@gmail.com>
2016-01-05 21:48:45 +0800

    Parsing URLs with the DOM

--- src/lib/tool/url.js
+++ src/lib/tool/url.js
@@ -25,6 +25,7 @@ define(function(require, exports, module) {
             return url;
         },
 
+
         forward: function(url) {
             setTimeout(function() {
                 window.location.href = url;
@@ -64,6 +65,41 @@ define(function(require, exports, module) {
                 }
             }
             return str_params;
+        },
+        /**
+         * Parsing URLs with the DOM
+         * http://james.padolsey.com/javascript/parsing-urls-with-the-dom/
+         */
+        parse: function(url) {
+            var a = document.createElement('a');
+            a.href = url;
+            return {
+                source: url,
+                protocol: a.protocol.replace(':', ''),
+                host: a.hostname,
+                port: a.port,
+                query: a.search,
+                params: (function() {
+                    var ret = {},
+                        seg = a.search.replace(/^\?/, '').split('&'),
+                        len = seg.length,
+                        i = 0,
+                        s;
+                    for (; i < len; i++) {
+                        if (!seg[i]) {
+                            continue;
+                        }
+                        s = seg[i].split('=');
+                        ret[s[0]] = s[1];
+                    }
+                    return ret;
+                })(),
+                file: (a.pathname.match(/([^/?#]+)$/i) || [0, ''])[1],
+                hash: a.hash.replace('#', ''),
+                path: a.pathname.replace(/^([^/])/, '/$1'),
+                relative: (a.href.match(/tps?:\/[^/]+(.+)/) || [0, ''])[1],
+                segments: a.pathname.replace(/^\//, '').split('/')
+            };
         }
     };
 
