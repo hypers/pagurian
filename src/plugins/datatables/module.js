70c6c494fcc039034ff90fddcebe88df8acc9906
simonguo <simonguo.2009@gmail.com>
2016-01-06 15:20:47 +0800

    datatables 添加clearTable方法

--- src/plugins/datatables/module.js
+++ src/plugins/datatables/module.js
@@ -9,346 +9,10 @@ define(function(require, exports, module) {
 
     require('./1.9.4/jquery.dataTables');
 
-    /* Set the defaults for DataTables initialisation */
-    $.extend(true, $.fn.dataTable.defaults, {
-        "sDom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>", // horizobtal scrollable datatable
-        //"sDom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>", // defaukt datatable without  horizobtal scroll
-        "sPaginationType": "bootstrap",
-        "oLanguage": {
-            "sLengthMenu": "_MENU_ records"
-        }
-    });
-
-
-    /* Default class modification */
-    $.extend($.fn.dataTableExt.oStdClasses, {
-        "sWrapper": "dataTables_wrapper form-inline"
-    });
-
-    /* API method to get paging information */
-    $.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings) {
-        return {
-            "iStart": oSettings._iDisplayStart,
-            "iEnd": oSettings.fnDisplayEnd(),
-            "iLength": oSettings._iDisplayLength,
-            "iTotal": oSettings.fnRecordsTotal(),
-            "iFilteredTotal": oSettings.fnRecordsDisplay(),
-            "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
-            "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
-        };
-    };
-
-
-    $.fn.dataTableExt.oApi.fnReloadAjax = function(oSettings, sNewSource, fnCallback, bStandingRedraw) {
-
-
-        if ($.fn.dataTable.versionCheck) {
-            var api = new $.fn.dataTable.Api(oSettings);
-
-            if (sNewSource) {
-                api.ajax.url(sNewSource).load(fnCallback, !bStandingRedraw);
-            } else {
-                api.ajax.reload(fnCallback, !bStandingRedraw);
-            }
-            return;
-        }
-
-        if (sNewSource !== undefined && sNewSource !== null) {
-            oSettings.sAjaxSource = sNewSource;
-        }
-
-        // Server-side processing should just call fnDraw
-        if (oSettings.oFeatures.bServerSide) {
-            this.fnDraw();
-            return;
-        }
-
-        this.oApi._fnProcessingDisplay(oSettings, true);
-        var that = this;
-        var iStart = oSettings._iDisplayStart;
-        var aData = [];
-
-        this.oApi._fnServerParams(oSettings, aData);
-
-        oSettings.fnServerData.call(oSettings.oInstance, oSettings.sAjaxSource, aData, function(json) {
-
-
-            that.oApi._fnClearTable(oSettings);
-
-            var aData = (oSettings.sAjaxDataProp !== "") ?
-                that.oApi._fnGetObjectDataFn(oSettings.sAjaxDataProp)(json) : json;
-
-            if (!aData) {
-                return;
-            }
-
-            for (var i = 0; i < aData.length; i++) {
-                that.oApi._fnAddData(oSettings, aData[i]);
-            }
-
-            oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
-
-            that.fnDraw();
-
-            if (bStandingRedraw === true) {
-                oSettings._iDisplayStart = iStart;
-                that.oApi._fnCalculateEnd(oSettings);
-                that.fnDraw(false);
-            }
-
-            that.oApi._fnProcessingDisplay(oSettings, false);
-
-            if (typeof fnCallback === 'function') {
-                fnCallback(oSettings);
-            }
-        }, oSettings);
-    };
-
-    $.fn.dataTableExt.oApi.fnLengthChange = function(oSettings, iDisplay) {
-        oSettings._iDisplayLength = iDisplay;
-        oSettings.oApi._fnCalculateEnd(oSettings);
-
-        /* If we have space to show extra rows (backing up from the end point - then do so */
-        if (oSettings._iDisplayEnd === oSettings.aiDisplay.length) {
-            oSettings._iDisplayStart = oSettings._iDisplayEnd - oSettings._iDisplayLength;
-            if (oSettings._iDisplayStart < 0) {
-                oSettings._iDisplayStart = 0;
-            }
-        }
-
-        if (oSettings._iDisplayLength === -1) {
-            oSettings._iDisplayStart = 0;
-        }
-
-        oSettings.oApi._fnDraw(oSettings);
-
-        if (oSettings.aanFeatures.l) {
-            $('select', oSettings.aanFeatures.l).val(iDisplay);
-        }
-    };
-
-    /* Bootstrap style pagination control */
-    $.extend($.fn.dataTableExt.oPagination, {
-        "bootstrap": {
-            "fnInit": function(oSettings, nPaging, fnDraw) {
-                var oLang = oSettings.oLanguage.oPaginate;
-                var fnClickHandler = function(e) {
-                    e.preventDefault();
-                    if (oSettings.oApi._fnPageChange(oSettings, e.data.action)) {
-                        fnDraw(oSettings);
-                    }
-                };
-
-                // pagination with prev, next link icons
-                $(nPaging).append(
-                    '<ul class="pagination">' +
-                    '<li class="prev disabled"><a href="#" title="' + oLang.sPrevious + '"><i class="fa fa-angle-left"></i></a></li>' +
-                    '<li class="next disabled"><a href="#" title="' + oLang.sNext + '"><i class="fa fa-angle-right"></i></a></li>' +
-                    '</ul>'
-                );
-
-                var els = $('a', nPaging);
-                $(els[0]).bind('click.DT', {
-                    action: "previous"
-                }, fnClickHandler);
-                $(els[1]).bind('click.DT', {
-                    action: "next"
-                }, fnClickHandler);
-            },
-
-            "fnUpdate": function(oSettings, fnDraw) {
-                var iListLength = 5;
-                var oPaging = oSettings.oInstance.fnPagingInfo();
-                var an = oSettings.aanFeatures.p;
-                var i, j, sClass, iStart, iEnd, iHalf = Math.floor(iListLength / 2);
-
-                if (oPaging.iTotalPages < iListLength) {
-                    iStart = 1;
-                    iEnd = oPaging.iTotalPages;
-                } else if (oPaging.iPage <= iHalf) {
-                    iStart = 1;
-                    iEnd = iListLength;
-                } else if (oPaging.iPage >= (oPaging.iTotalPages - iHalf)) {
-                    iStart = oPaging.iTotalPages - iListLength + 1;
-                    iEnd = oPaging.iTotalPages;
-                } else {
-                    iStart = oPaging.iPage - iHalf + 1;
-                    iEnd = iStart + iListLength - 1;
-                }
-
-                if (oPaging.iTotalPages < 0) {
-                    $('.pagination', an[i]).css('visibility', 'hidden');
-                } else {
-                    $('.pagination', an[i]).css('visibility', 'visible');
-                }
-
-
-
-
-                for (i = 0, iLen = an.length; i < iLen; i++) {
-                    // Remove the middle elements
-                    $('li:gt(0)', an[i]).filter(':not(:last)').remove();
-
-                    // Add the new list items and their event handlers
-                    for (j = iStart; j <= iEnd; j++) {
-
-                        sClass = (j === oPaging.iPage + 1) ? 'class="active"' : '';
-
-                        $('<li ' + sClass + '><a href="#">' + j + '</a></li>')
-                            .insertBefore($('li:last', an[i])[0])
-                            .bind('click', function(e) {
-
-                                e.preventDefault();
-                                oSettings._iDisplayStart = (parseInt($('a', this).text(), 10) - 1) * oPaging.iLength;
-                                fnDraw(oSettings);
-
-                            });
-                    }
-
-                    // Add / remove disabled classes from the static elements
-                    if (oPaging.iPage === 0) {
-                        $('li:first', an[i]).addClass('disabled');
-                    } else {
-                        $('li:first', an[i]).removeClass('disabled');
-                    }
-
-                    if (oPaging.iPage === oPaging.iTotalPages - 1 || oPaging.iTotalPages === 0) {
-                        $('li:last', an[i]).addClass('disabled');
-                    } else {
-                        $('li:last', an[i]).removeClass('disabled');
-                    }
-                }
-            }
-        }
-    });
-
-    /* Bootstrap style full number pagination control */
-    $.extend($.fn.dataTableExt.oPagination, {
-        "bootstrap_full_number": {
-            "fnInit": function(oSettings, nPaging, fnDraw) {
-                var oLang = oSettings.oLanguage.oPaginate;
-                var fnClickHandler = function(e) {
-
-                    e.preventDefault();
-                    if (oSettings.oApi._fnPageChange(oSettings, e.data.action)) {
-                        fnDraw(oSettings);
-                    }
-                };
-
-                $(nPaging).append(
-                    '<ul class="pagination">' +
-                    '<li class="prev disabled"><a href="#" title="' + oLang.sFirst + '"><i class="fa fa-angle-double-left"></i></a></li>' +
-                    '<li class="prev disabled"><a href="#" title="' + oLang.sPrevious + '"><i class="fa fa-angle-left"></i></a></li>' +
-                    '<li class="next disabled"><a href="#" title="' + oLang.sNext + '"><i class="fa fa-angle-right"></i></a></li>' +
-                    '<li class="next disabled"><a href="#" title="' + oLang.sLast + '"><i class="fa fa-angle-double-right"></i></a></li>' +
-                    '</ul>'
-                );
-                var els = $('a', nPaging);
-                $(els[0]).bind('click.DT', {
-                    action: "first"
-                }, fnClickHandler);
-                $(els[1]).bind('click.DT', {
-                    action: "previous"
-                }, fnClickHandler);
-                $(els[2]).bind('click.DT', {
-                    action: "next"
-                }, fnClickHandler);
-                $(els[3]).bind('click.DT', {
-                    action: "last"
-                }, fnClickHandler);
-            },
-
-            "fnUpdate": function(oSettings, fnDraw) {
-
-                var iListLength = 5;
-                var oPaging = oSettings.oInstance.fnPagingInfo();
-                var an = oSettings.aanFeatures.p;
-                var i, j, sClass, iStart, iEnd, iHalf = Math.floor(iListLength / 2);
-
-                if (oPaging.iTotalPages < iListLength) {
-                    iStart = 1;
-                    iEnd = oPaging.iTotalPages;
-                } else if (oPaging.iPage <= iHalf) {
-                    iStart = 1;
-                    iEnd = iListLength;
-                } else if (oPaging.iPage >= (oPaging.iTotalPages - iHalf)) {
-                    iStart = oPaging.iTotalPages - iListLength + 1;
-                    iEnd = oPaging.iTotalPages;
-                } else {
-                    iStart = oPaging.iPage - iHalf + 1;
-                    iEnd = iStart + iListLength - 1;
-                }
-
-                if (oPaging.iTotalPages < 0) {
-                    $('.pagination', an[i]).css('visibility', 'hidden');
-                } else {
-                    $('.pagination', an[i]).css('visibility', 'visible');
-                }
-
-                for (i = 0, iLen = an.length; i < iLen; i++) {
-                    // Remove the middle elements
-                    $('li:gt(1)', an[i]).filter(':not(.next)').remove();
-
-                    // Add the new list items and their event handlers
-                    for (j = iStart; j <= iEnd; j++) {
-                        sClass = (j === oPaging.iPage + 1) ? 'class="active"' : '';
-                        $('<li ' + sClass + '><a href="#">' + j + '</a></li>')
-                            .insertBefore($('li.next:first', an[i])[0])
-                            .bind('click', function(e) {
-                                e.preventDefault();
-                                oSettings._iDisplayStart = (parseInt($('a', this).text(), 10) - 1) * oPaging.iLength;
-                                fnDraw(oSettings);
-                            });
-                    }
-
-                    // Add / remove disabled classes from the static elements
-                    if (oPaging.iPage === 0) {
-                        $('li.prev', an[i]).addClass('disabled');
-                    } else {
-                        $('li.prev', an[i]).removeClass('disabled');
-                    }
-
-                    if (oPaging.iPage === oPaging.iTotalPages - 1 || oPaging.iTotalPages === 0) {
-                        $('li.next', an[i]).addClass('disabled');
-                    } else {
-                        $('li.next', an[i]).removeClass('disabled');
-                    }
-                }
-            }
-        }
-    });
-
-
-    /*
-     * TableTools Bootstrap compatibility
-     * Required TableTools 2.1+
-     */
-    if ($.fn.DataTable.TableTools) {
-        // Set the classes that TableTools uses to something suitable for Bootstrap
-        $.extend(true, $.fn.DataTable.TableTools.classes, {
-            "container": "btn-group",
-            "buttons": {
-                "normal": "btn default",
-                "disabled": "btn disabled"
-            },
-            "collection": {
-                "container": "DTTT_dropdown dropdown-menu",
-                "buttons": {
-                    "normal": "",
-                    "disabled": "disabled"
-                }
-            }
-        });
-
-        // Have the collection use a bootstrap compatible dropdown
-        $.extend(true, $.fn.DataTable.TableTools.DEFAULTS.oTags, {
-            "collection": {
-                "container": "ul",
-                "button": "li",
-                "liner": "a"
-            }
-        });
-    }
+    require('./extend/options');
+    require('./extend/functions');
+    require('./extend/pagination');
+    require('./extend/compatibility');
 
     function DataTables(seletor, options) {
 
@@ -359,7 +23,7 @@ define(function(require, exports, module) {
         this.bLoadFinish = false;
         this.bShowSummary = false;
         this.aApiParams = {};
-        this.version = "0.1.1208";
+        this.version = "0.2.160106";
         this.options = {
             "sForm": '#form',
             "bAutoload": true,
@@ -540,7 +204,7 @@ define(function(require, exports, module) {
                             //生成序号
                             var bOrderNumbers = oSettings.oInit.bOrderNumbers || oSettings.oInit.isCreateOrder;
                             if (bOrderNumbers && items.length) {
-                                createNumber(a.page.current, a.page.pagesize);
+                                createOrderNumbers(a.page.current, a.page.pagesize);
                             }
 
                             //汇总信息
@@ -669,7 +333,7 @@ define(function(require, exports, module) {
         this.init = function() {
 
             var aoColumns;
-
+            this.container = $(seletor);
             $.extend(true, this.options, options);
 
             //显示汇总信息
@@ -736,12 +400,20 @@ define(function(require, exports, module) {
             return this;
         };
 
+        //更新表格数据
         this.update = function() {
             this.table.fnPageChange(0);
             return this;
         };
 
-        function createNumber(current, pagesize) {
+        //清空表格数据
+        this.clearTable = function() {
+            $(seletor + " .table-summary").html("--");
+            this.table.fnClearTable();
+        };
+
+        //创建表格序号
+        function createOrderNumbers(current, pagesize) {
             var index = (current - 1) * pagesize,
                 k = 1;
             $(seletor).find("tbody tr").each(function() {
