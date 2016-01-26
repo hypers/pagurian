c24679d165f73a239eb3a43fad5d5dd308286400
Godefery <hiyangguo@qq.com>
2016-01-26 15:38:08 +0800

    重构了sizer组件 修改ui

--- src/modules/sizer/app.js
+++ src/modules/sizer/app.js
@@ -2,37 +2,35 @@
  * Created by yangguo on 2015/10/13 0013.
  */
 define(function (require, exports, module) {
-    window.CONFIG={
-        appId:"Sizer"
-    };
     var app = require("../../lib/app");
     var model = require('./model');
 
     require('../../widgets/sizer/module');
 
+    /**
+     * 多选按钮
+     */
     app.page.multiple = function () {
         var selectDatas = [{
             "id": 4
-        },{
+        }, {
             "id": 5
         }], options = {
             isMultiple: true,//是否为多选 默认为false
             isExpand: true,//是否默认展开 默认为false
             dataSource: model.getSizerData,//数据源
             style: "d-ib", //筛选器自定义class
-            //processing: oLanguage.processing,//loading默认文字
-            //search: oLanguage.search,//搜索框默认文字
             callbackExpand: function () {//面板展开时的回调
                 console.log("Expand");
             },
-            callbackClose: function (datas,allDatas) {//面板关闭时的回掉
+            callbackClose: function (datas, allDatas) {//面板关闭时的回掉
                 console.log("Close");
                 console.log(datas);
                 console.log(allDatas);
             },
-            callbackOption: function (data) {//点击选项的回调
-                console.log(data);
-            },
+            //callbackOption: function (data) {//点击选项的回调
+            //    console.log(data);
+            //},
             callbackSearch: function (datas) {//搜索框录入回调
                 console.log(datas);
             },
@@ -43,7 +41,7 @@ define(function (require, exports, module) {
             callbackSelectAll: function (datas) {//全选时的回调
                 console.log("SelectAll");
             },
-            callbackSubmit: function (selectDatas,allDatas) {//确认按钮回调
+            callbackSubmit: function (selectDatas, allDatas) {//确认按钮回调
                 console.log("Submit");
                 console.log(selectDatas);
                 console.log(allDatas);
@@ -52,9 +50,15 @@ define(function (require, exports, module) {
                 console.log("Cancel");
             }
         };
-        var sizer = $p.sizer("#sizer-multiple", options, selectDatas);
+        var sizer = $p.sizer($("#sizer-multiple"), options, selectDatas);
+        console.log(sizer.onBind("callbackOption", function (data) {
+            console.log(data);
+        }));
     };
 
+    /**
+     * 单选按钮
+     */
     app.page.single = function () {
         var selectDatas = [{
             "id": 4
@@ -69,7 +73,7 @@ define(function (require, exports, module) {
             callbackExpand: function () {//面板展开时的回调
                 console.log("Expand");
             },
-            callbackClose: function (datas,allDatas) {//面板关闭时的回掉
+            callbackClose: function (datas, allDatas) {//面板关闭时的回掉
                 console.log("Close");
                 console.log(datas);
                 console.log(allDatas);
