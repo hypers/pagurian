feba889ba6eb41d4ee7f0584a538e43611d30de3
Godfery <hiyangguo@qq.com>
2016-10-12 10:45:25 +0800

    增加echarts 桑基图 example

---
+++ src/modules/echarts-sankey/app.js
@@ -0,0 +1,84 @@
+define(function (require, exports, module) {
+
+    var app = require("../../lib/app");
+    require("../../plugins/echarts/module");
+    var chart = {};
+    app.page.sankey = function () {
+
+        chart = $p.echarts("my_chart", {
+            type: "sankey",
+            title: {
+                text: '来源分析'
+            }
+        });
+
+        chart.load({
+            data: [
+                {
+                    "source": "首页",
+                    "target": "签到领金币",
+                    "zlevel": 0,
+                    "value": 83034
+                },
+                {
+                    "source": "首页",
+                    "target": "进入iBuick",
+                    "value": 8098
+                },
+                {
+                    "source": "首页",
+                    "target": "油费随手记",
+                    "value": 6868
+                },
+                {
+                    "source": "首页",
+                    "target": "违章查询",
+                    "value": 4336
+                },
+                {
+                    "source": "签到领金币",
+                    "target": "幸运大抽奖",
+                    "value": 61785,
+                    "zlevel": 1
+                },
+                {
+                    "source": "签到领金币",
+                    "target": "进入个人中心",
+                    "value": 1797
+                },
+                {
+                    "source": "签到领金币",
+                    "target": "其它",
+                    "value": 899
+                },
+                {
+                    "source": "签到领金币",
+                    "target": "其它",
+                    "zlevel": 0,
+                    "value": 83034
+                },
+                {
+                    "source": "进入iBuick",
+                    "target": "其它",
+                    "value": 8098
+                },
+                {
+                    "source": "油费随手记",
+                    "target": "其它",
+                    "value": 6868
+                },
+                {
+                    "source": "违章查询",
+                    "target": "其它",
+                    "value": 4336
+                }
+            ]
+        });
+
+
+    };
+
+
+    module.exports = app;
+
+});
--- src/plugins/echarts/chart/sankey.js
+++ src/plugins/echarts/chart/sankey.js
@@ -6,7 +6,26 @@ define(function (require, exports, module) {
         zh_CN: require('../locale/zh_CN'),
         en_US: require('../locale/en_US')
     };
+
+    /**
+     *
+     * @param {Object} options
+     * @param {Array} options.data - 数据
+     * @return {{title: {x: string, y: string, textStyle: {color: string, fontWeight: string}}, tooltip: {trigger: string, triggerOn: string}, series: *[]}}
+     */
     module.exports = function (options) {
+        var links = options.data || [];
+        var nodes = {};
+
+        links.forEach(function (data) {
+            addKeyToNode(data.source);
+            addKeyToNode(data.target);
+        });
+
+        nodes = Object.keys(nodes).map(function (key) {
+            return {name: key};
+        });
+
         var option = {
             title: {
                 x: "center",
@@ -24,12 +43,37 @@ define(function (require, exports, module) {
                 {
                     type: 'sankey',
                     layout: 'none',
-                    data: [],
-                    links: [],
+                    layoutIterations: 0,
+                    data: nodes,
+                    links: links,
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: function (params) {
+                            return [options.name, params.name + '：' + (isNaN(params.value) ? '-' : params.value)].join('<br/>');
+                        }
+                    },
                     itemStyle: {
                         normal: {
                             borderWidth: 1,
-                            borderColor: '#aaa'
+                            borderColor: '#aaa',
+                            label: {
+                                show: true,
+                                textStyle: {
+                                    fontSize: pagurian.language === "en_US" ? "10" : "12"
+                                }
+                            }
+                        },
+                        emphasis: {
+                            label: {
+                                show: true,
+                                textStyle: {
+                                    color: "#eee"
+                                }
+                            },
+                            areaStyle: {
+                                color: '#d7504b'
+                            },
+                            areaColor: '#d7504b'
                         }
                     },
                     lineStyle: {
@@ -41,6 +85,17 @@ define(function (require, exports, module) {
                 }
             ]
         };
+
+        /**
+         * 添加key到node
+         * @param {String} key
+         */
+        function addKeyToNode(key) {
+            if (nodes[key]) {
+                return;
+            }
+            nodes[key] = true;
+        }
 
         return option;
     };
